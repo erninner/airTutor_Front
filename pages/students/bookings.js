c9a8ba1b926469bb9176c0958c24d9390e054f80
wei cui <weicui.cuiwei@gmail.com>
2021-11-23 18:38:48 +0800

    create-student-booking-page

---
+++ pages/students/bookings.js
@@ -0,0 +1,66 @@
+// pages/students/bookings.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Cnavigatorlled when pnavigatorge lonavigatord
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Cnavigatorlled when pnavigatorge is initinavigatorlly rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Cnavigatorlled when pnavigatorge show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Cnavigatorlled when pnavigatorge hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Cnavigatorlled when pnavigatorge unlonavigatord
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Pnavigatorge event hnavigatorndler function--Cnavigatorlled when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Cnavigatorlled when pnavigatorge renavigatorch bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Cnavigatorlled when user click on the top right corner to shnavigatorre
+   */
+  onShareappMessage: function () {
+
+  }
+})
