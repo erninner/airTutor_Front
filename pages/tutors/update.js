99d006dc99f3d3714c379392ec1ac8badf851304
Gary Shih <garyjshih@gmail.com>
2021-11-23 15:19:38 +0800

    added pages tutors_update

--- pages/tutors/update.js
+++ pages/tutors/update.js
@@ -1,4 +1,4 @@
-// pages/tutors/bookings.js
+// pages/tutors/update.js
 Page({
 
   /**
@@ -7,28 +7,6 @@ Page({
   data: {
 
   },
-  // BOOKINGS FROM STUDENTS TO BE ADDED TO TEACHERS BOOKINGS VIEW
-  // formSubmit(e){
-  //   // console.log(e)
-  //   let name = e.detail.value.name
-  //   let text = e.detail.value.text
-  //   // data.stories.push({title: name, details: text})
-  //   wx.request ({
-  //     url: `${url}/stories`,
-  //     method: `POST`,
-  //     data: {name: name, text: text}
-  //   })
-
-  //   // debugger
-  //   wx.showToast({
-  //     title: 'Added',
-  //   })
-  //   setTimeout(() => {
-  //     wx.switchTab({
-  //       url: '/pages/stories/stories',
-  //     })
-  //   }, 1000); 
-  // },
 
   /**
    * Lifecycle function--Called when page load
