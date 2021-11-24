1a2c1ed698cde6f935669367c55bfcc197ebc0c3
Terence Yuan <yuanlei_cam@hotmail.com>
2021-11-24 18:06:17 +0800

    card added

--- components/horizontal_card/horizontal_card.js
+++ components/horizontal_card/horizontal_card.js
@@ -13,7 +13,7 @@ Component({
   data: {
     tutors:
     [
-    	{ name: "Terence", credential: "Le Wagon Student", url:"/assets/images/terence.png"},
+    	{ name: "Terence", credential: "Professional Tutor", url:"/assets/images/terence.png"},
     	{ name: "Xun", credential: "Full-stack", url:"https://kitt.lewagon.com/placeholder/users/Eschults" },
     	{ name: "Pavel", credential: "Data Pro", url:"https://kitt.lewagon.com/placeholder/users/krokrob"},
     	{ name: "Alex", credential: "Bossy boss", url:"https://kitt.lewagon.com/placeholder/users/arthur-littm" },
