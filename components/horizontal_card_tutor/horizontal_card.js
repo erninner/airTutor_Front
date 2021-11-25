21de0fbf8871fe2e15fcfd221b98a095fe692ded
Terence Yuan <yuanlei_cam@hotmail.com>
2021-11-25 14:33:57 +0800

    cleaned version of pages

---
+++ components/horizontal_card_tutor/horizontal_card.js
@@ -0,0 +1,34 @@
+// assets/horizontal_card/horizontal_card.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    tutors:
+    [
+    	{ name: "Terence", credential: "Professional Tutor", url:"/assets/images/terence.png"},
+    	{ name: "Xun", credential: "Full-stack", url:"https://kitt.lewagon.com/placeholder/users/Eschults" },
+    	{ name: "Pavel", credential: "Data Pro", url:"https://kitt.lewagon.com/placeholder/users/krokrob"},
+    	{ name: "Alex", credential: "Bossy boss", url:"https://kitt.lewagon.com/placeholder/users/arthur-littm" },
+    	{ name: "Michael", credential: "Gin Pro", url:"https://unsplash.com/photos/WNoLnJo7tS8" },
+    ]
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    tap: function(env){
+      wx.navigateTo({
+        url: '/pages/tutors/bookings'
+      })
+    }
+  }
+})
